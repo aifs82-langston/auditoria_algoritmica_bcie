71e07e77b1bbe5793b2feaa3a89a3aa72da8b11c
aifs82-langston <aifs82@gmail.com>
2026-01-24 12:40:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ sns.set(style="whitegrid", context="talk")
 st.title("🏦 Auditoría Algorítmica de los desembolsos del BCIE para los países fundadores")
 st.markdown("""
 **Auditoría Algorítmica de Datos Abiertos:**
-Esta aplicación conecta en tiempo real a las APIs del BCIE y del SDG Index, aplicando técnicas de vectorización semántica (S-BERT), minería de texto y aprendizaje no supervisado para auditar la estructura funcional y financiera de la cartera de proyectos.
+Esta aplicación conecta en tiempo real a las APIs del BCIE y del SDG Index, aplicando técnicas de vectorización semántica (S-BERT), minería de texto y aprendizaje no supervisado para auditar la estructura funcional y financiera de la cartera de proyectos de los países fundadores para el período 2010-2024.
 """)
 
 # ==========================================

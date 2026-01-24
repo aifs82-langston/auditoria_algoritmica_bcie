c4434bce00e28d270066205bc23c2ba1da9fefdd
aifs82-langston <aifs82@gmail.com>
2026-01-24 11:47:14 -0600

    Update app.py

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ if st.button('🚀 EJECUTAR AUDITORÍA COMPLETA', type="primary"):
         buffer = io.BytesIO()
         with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
             df_bcie.to_excel(writer, sheet_name='Data_Cruda', index=False)
-            pivot_monto_millones.to_excel(writer, sheet_name='Matriz_Vpromedio')
+            pivot_monto_millones.to_excel(writer, sheet_name='Matriz_Montos')
 
         st.download_button(
             label="💾 Descargar Resultados Financieros (Excel)",
@@ -252,18 +252,7 @@ if st.button('🚀 EJECUTAR AUDITORÍA COMPLETA', type="primary"):
         ax3.set_xticklabels(ax3.get_xticklabels(), rotation=45, ha='right')
         st.pyplot(fig3)
 
-        # Botón descarga
-        buffer = io.BytesIO()
-        with pd.ExcelWriter(buffer, engine='xlsxwriter') as writer:
-            df_bcie.to_excel(writer, sheet_name='Data_Cruda', index=False)
-            pivot_monto_millones.to_excel(writer, sheet_name='Matriz_Montos')
 
-        st.download_button(
-            label="💾 Descargar Resultados Valor Promedio (Excel)",
-            data=buffer,
-            file_name="BCIE_ValorMedio_Resultados.xlsx",
-            mime="application/vnd.ms-excel"
-        )
 
         #st.markdown("""
         #**Hallazgo Clave:** * **Azul Intenso:** Operaciones de gran escala (ej. Apoyo Presupuestario en Costa Rica).
